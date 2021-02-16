82e13ce7c675f71699f4ef84b17ad548d02232c9
Andres Icedo <andresicedo24@gmail.com>
2021-02-16 09:43:44 -0600

    completed project

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
-	// code here will execute after the document is loaded
 	const moviesContainer = document.querySelector(".movies-container");
 	const searchForm = document.querySelector('#search-form');
 	searchForm.addEventListener('submit', function (e) {
 		e.preventDefault();
-
-
-
 		let searchString = document.querySelector(".search-bar").value; 
 		let urlEncodedSearchString = encodeURIComponent(searchString);
 		axios.get(`http://www.omdbapi.com/?apikey=59354c85&s=${urlEncodedSearchString}`)
 		.then(function(response) {
-			console.log(response.data);
 			var movieHTML = renderMovies(response.data.Search);
 			moviesContainer.innerHTML = movieHTML;
 			movieData = response.data.Search;
 		});
-
-
 	})
 });
 
@@ -49,14 +42,10 @@ function saveToWatchlist(imdbID) {
 	const movie = movieData.find((currentMovie) => currentMovie.imdbID == imdbID);
 	let watchlistJSON = localStorage.getItem('watchlist');
 	let watchlist = JSON.parse(watchlistJSON);
-	//if the watchlist is null, set to an empty array
 	if (watchlist === null) {
 		watchlist = [];
 	};
-		//Push movie into the watchlist
 		watchlist.push(movie);
-		//Turn the watchlist back into JSON
 		watchlistJSON = JSON.stringify(watchlist);
-		//Save the JSONified watchlist back into local storage
 		localStorage.setItem('watchlist', watchlistJSON);
 }
